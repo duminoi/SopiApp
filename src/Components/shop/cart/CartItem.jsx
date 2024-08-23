fe18be85a1adb902b7b39ebac1765bcc938fa118
duminoi <dminso1koaiso2@gmail.com>
2024-08-23 14:22:48 +0700

    u

--- src/Components/shop/cart/CartItem.jsx
+++ src/Components/shop/cart/CartItem.jsx
@@ -10,11 +10,11 @@ export default function CartItem({ name, price, quantity, order }) {
       <tr className="text-gray-700">
         <td className="px-4 py-3">
           <div className="flex items-center text-sm">
-            <p className="font-semibold">{name}</p>
+            <p className="font-bold">{name}</p>
           </div>
         </td>
         <td className="px-4 py-3 text-sm">{order}</td>
-        <td className="px-4 py-3 text-sm">{quantity}</td>
+        <td className="px-4 py-3 text-sm">{quantity - order}</td>
         <td className="px-4 py-3 text-sm">{price * order}</td>
       </tr>
     </>
